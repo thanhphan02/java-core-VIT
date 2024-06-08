4bc94cd9e48ad1a5c6ccc26547a438dab6e6fb0d
thanhphan02 <quangthanhphan02@gmail.com>
2024-06-08 19:28:02 +0700

    Lear method syntax

---
+++ src/main/java/MethodDemo.java
@@ -0,0 +1,8 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println("dog.isPrime(11)= " + dog.isPrime(11));
+        System.out.println("dog.isPrime(15)= " + dog.isPrime(15));
+
+    }
+}
